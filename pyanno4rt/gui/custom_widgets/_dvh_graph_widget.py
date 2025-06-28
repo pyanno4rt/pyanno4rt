d905e787674aca97c7bc36ead13e28ff7797ffc1
Tim Ortkamp <tim.ortkamp@gmx.de>
2025-06-29 00:36:55 +0200

    Fixed small error in DVH graph widget with colormap selection

--- pyanno4rt/gui/custom_widgets/_dvh_graph_widget.py
+++ pyanno4rt/gui/custom_widgets/_dvh_graph_widget.py
@@ -86,7 +86,8 @@ class DVHGraphWidget(QWidget):
             if segment in dose_histogram['display_segments'])
 
         # Set the colormap
-        colors = colormap.get('tab20b').getLookupTable(nPts=len(self.segments))
+        colors = colormap.get(
+            'tab20b', 'matplotlib').getLookupTable(nPts=len(self.segments))
 
         # Set the line styles
         linestyles = tuple(islice(
